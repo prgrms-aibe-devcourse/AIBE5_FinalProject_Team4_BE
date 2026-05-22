5c4d1a1ff4811cb5aa8bcc3a54383c42ab0d7d79
devken65 <effortcoders@gmail.com>
2026-05-22 17:45:31 +0900

    chore: 프로젝트 초기세팅

---
+++ src/main/java/com/closetnangam/be/ClosetnangamBeApplication.java
@@ -0,0 +1,13 @@
+package com.closetnangam.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosetnangamBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClosetnangamBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/closetnangam/be/ClosetnangamBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.closetnangam.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClosetnangamBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
