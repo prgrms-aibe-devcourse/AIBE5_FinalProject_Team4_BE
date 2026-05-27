7656d8837081728a0016451b3bafa965211a38fb
devken65 <effortcoders@gmail.com>
2026-05-27 18:28:05 +0900

    comment: 주석 한줄추가

--- src/main/java/com/closetnangam/be/ClosetnangamBeApplication.java
+++ src/main/java/com/closetnangam/be/ClosetnangamBeApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+// 아무런 변경
 @SpringBootApplication
 @EnableJpaAuditing
 public class ClosetnangamBeApplication {
