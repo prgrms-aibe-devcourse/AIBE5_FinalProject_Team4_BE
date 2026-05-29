0d7a0ba3ca7bce1d97d15b0e55d44d1672fff3bc
warcat12 <warcat12@gmail.com>
2026-05-29 12:16:45 +0900

    feat/ #33 코드 리뷰 반영

--- src/main/java/com/closetnangam/be/domain/user/repository/UserRepository.java
+++ src/main/java/com/closetnangam/be/domain/user/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.closetnangam.be.domain.user.repository;
 
 import com.closetnangam.be.domain.user.entity.User;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -10,4 +14,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     boolean existsByNickname(String nickname);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT u FROM User u WHERE u.id = :userId")
+    Optional<User> findByIdForUpdate(@Param("userId") Long userId);
 }
