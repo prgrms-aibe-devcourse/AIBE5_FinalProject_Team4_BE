0d2f3b0c12063235774f7dec4475a55d02bd80df
이석민 <dltjrals1018@naver.com>
2026-06-01 17:06:12 +0900

    feat/#40 추천 로직 미완

--- src/main/java/com/closetnangam/be/domain/clothes/repository/ClothesRepository.java
+++ src/main/java/com/closetnangam/be/domain/clothes/repository/ClothesRepository.java
@@ -1,7 +1,27 @@
 package com.closetnangam.be.domain.clothes.repository;
 
 import com.closetnangam.be.domain.clothes.entity.Clothes;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ClothesRepository extends JpaRepository<Clothes, Long> {
+    Optional<Clothes> findByExternalProductId(String externalProductId);
+
+
+    @Query("""
+    SELECT s.name, COUNT(cst.id) as styleCount
+    FROM WardrobeClothes wc
+    JOIN wc.clothes c
+    JOIN c.styleTags cst
+    JOIN cst.style s
+    WHERE wc.wardrobe.id = :wardrobeId
+    GROUP BY s.name
+    ORDER BY styleCount DESC
+    """)
+    List<Object[]> countStyleTagsByWardrobe(@Param("wardrobeId") Long wardrobeId, Pageable pageable);
 }
