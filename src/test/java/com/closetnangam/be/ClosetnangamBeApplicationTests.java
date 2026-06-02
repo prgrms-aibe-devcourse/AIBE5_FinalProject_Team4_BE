ad5f153df298c08a45e5a6cfdf7f8a0f0238efda
류태우 <vlvkapsldk@naver.com>
2026-06-02 12:02:46 +0900

    fix/#37 유사상품추천

--- src/test/java/com/closetnangam/be/ClosetnangamBeApplicationTests.java
+++ src/test/java/com/closetnangam/be/ClosetnangamBeApplicationTests.java
@@ -3,14 +3,13 @@ package com.closetnangam.be;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
-@TestPropertySource(properties = {
-        // H2는 MySQL dialect 없이 동작하므로 JPA dialect 자동 감지에 맡김
-        "spring.jpa.properties.hibernate.dialect="
-})
+/*
+ * CI는 application-test.yml의 MySQL 테스트 DB를 사용한다.
+ * Spring Boot 테스트 자동 설정이 datasource를 embedded DB로 교체하지 않도록 명시한다.
+ */
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ClosetnangamBeApplicationTests {
 
     @Test
